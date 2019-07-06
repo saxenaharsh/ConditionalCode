87a0ca71f6cb309a0eb3272aeebf6a44c1c735ed
saxenaharsh <harsh.saxena@gmail.com>
2019-07-06 14:38:14 +0530

    Add files via upload

--- ConditionalCodeBlocks/src/com/HarshSaxena/Main.java
+++ ConditionalCodeBlocks/src/com/HarshSaxena/Main.java
@@ -10,25 +10,27 @@ public class Main {
         int bonus = 100;
 
 
-
-        if(score < 5000 && score > 1000){
-            System.out.println("Your score is less than 5000 but greater than 1000");
-        }else if(score < 1000){
-            System.out.println("Your score is less than 1000");
-        }
-        else {
-            System.out.println("Got here");
-        }
-
         if(gameOver){
             int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
         }
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
-        System.out.println("Score is " + score + " Level completed is " + levelCompleted + " Bonus is " + bonus);
+
+
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus );
+            System.out.println("Your final socre was " + finalScore);
+        }
+
+
+
+
+
+
 
     }
 }
